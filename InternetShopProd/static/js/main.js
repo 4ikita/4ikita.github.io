9f1463141a4f66fce68b0632ade9510876f90390
4ikita <35740548+4ikita@users.noreply.github.com>
2020-07-03 13:50:08 +0500

    Update main.js

--- InternetShopProd/static/js/main.js
+++ InternetShopProd/static/js/main.js
@@ -3,8 +3,5 @@
 $(document).ready(function() {
 
     // Slider at the main page 
-    $('.slider').bxSlider({
-        pagerCustom: '.js-index-slider__pager',
-        controls: true
-    });
+    
 });
