80c10b6b5fc49be8ea71dc50a9c3672b69111fc3
4ikita <35740548+4ikita@users.noreply.github.com>
2020-07-03 13:42:46 +0500

    Add files via upload

---
+++ InternetShopProd/static/js/main.js
@@ -0,0 +1,10 @@
+;
+// Начинать писать отсюда!!!!
+$(document).ready(function() {
+
+    // Slider at the main page 
+    $('.slider').bxSlider({
+        pagerCustom: '.js-index-slider__pager',
+        controls: true
+    });
+});
